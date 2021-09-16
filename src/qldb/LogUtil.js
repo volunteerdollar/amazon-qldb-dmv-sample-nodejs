47190fd176f5ca60f5818356be35ff5ed2f5dbdd
Kyle <kyle.ueckermann@gmail.com>
2021-09-16 16:43:51 +0300

    Removed logs and compiled

--- src/qldb/LogUtil.js
+++ src/qldb/LogUtil.js
@@ -1,3 +1,4 @@
+"use strict";
 /*
  * Copyright 2019 Amazon.com, Inc. or its affiliates. All Rights Reserved.
  *
@@ -10,50 +11,50 @@
  * CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions
  * and limitations under the License.
  */
-
-import { config } from "aws-sdk";
-
+exports.__esModule = true;
+exports.isLoggerSet = exports.log = exports.error = void 0;
+var aws_sdk_1 = require("aws-sdk");
 // config.logger = console;
-
 /**
  * Logs an error level message.
  * @param line The message to be logged.
  */
-export function error(line: string): void {
+function error(line) {
     if (isLoggerSet()) {
         _prepend(line, "ERROR");
     }
 }
-
+exports.error = error;
 /**
  * Logs a message.
  * @param line The message to be logged.
  */
-export function log(line: string): void {
+function log(line) {
     if (isLoggerSet()) {
         _prepend(line, "LOG");
     }
 }
-
+exports.log = log;
 /**
  * @returns A boolean indicating whether a logger has been set within the AWS SDK.
  */
-export function isLoggerSet(): boolean {
-    return config.logger !== null;
+function isLoggerSet() {
+    return aws_sdk_1.config.logger !== null;
 }
-
+exports.isLoggerSet = isLoggerSet;
 /**
  * Prepends a string identifier indicating the log level to the given log message, & writes or logs the given message
  * using the logger set in the AWS SDK.
  * @param line The message to be logged.
  * @param level The log level.
  */
-function _prepend(line: string, level: string): void {
-    if (config.logger) {
-        if (typeof config.logger.log === "function") {
-            config.logger.log(`[${level}][Node.js QLDB Sample Code] ${line}`);
-        } else if (typeof config.logger.write === "function") {
-            config.logger.write(`[${level}][Node.js QLDB Sample Code] ${line}\n`);
+function _prepend(line, level) {
+    if (aws_sdk_1.config.logger) {
+        if (typeof aws_sdk_1.config.logger.log === "function") {
+            aws_sdk_1.config.logger.log("[" + level + "][Node.js QLDB Sample Code] " + line);
+        }
+        else if (typeof aws_sdk_1.config.logger.write === "function") {
+            aws_sdk_1.config.logger.write("[" + level + "][Node.js QLDB Sample Code] " + line + "\n");
         }
     }
 }
